831312da1ab32fdaef2a40931bf842deb2753f06
M. S. Rihan <zu7hunter@gmail.com>
2026-04-05 18:59:10 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (_req, res) => {
   res.json({
     message: "Welcome to the Web Scrape Bot API",
@@ -6,3 +15,6 @@ app.get("/", (_req, res) => {
     health: "/api/healthz",
   });
 });
+
+app.use("/api", router);
+export default app;
